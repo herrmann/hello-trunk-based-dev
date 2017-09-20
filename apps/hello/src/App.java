ca6b00f08022243113267d7df62012691b74b4ab
Ricardo Herrmann <ricardo.herrmann@dell.com>
2017-09-20 13:04:51 -0300

    Feature builder

--- apps/hello/src/App.java
+++ apps/hello/src/App.java
@@ -2,13 +2,16 @@ public class App {
 
 	public static void main(final String[] args) {
 		final AppConfig config = new AppConfig();
-		Greeter greeter;
-		if (config.useNewFeature()) {
-			greeter = new NewGreeter();
-		} else {
-			greeter = new OldGreeter();
-		}
+		final Greeter greeter = buildGreeter(config);
 		greeter.greeting();
+	}
+
+	private static Greeter buildGreeter(final AppConfig config) {
+		if (config.useNewFeature()) {
+			return new NewGreeter();
+		} else {
+			return new OldGreeter();
+		}
 	}
 
 }
