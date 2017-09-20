09267dfc97dcb2cd528f73f160dd5582c0ec4d72
Ricardo Herrmann <ricardo.herrmann@dell.com>
2017-09-20 15:10:33 -0300

    Use command line arguments

--- apps/hello/src/App.java
+++ apps/hello/src/App.java
@@ -2,7 +2,13 @@ public class App {
 
 	public static void main(final String[] args) {
 		final Greeter greeter = GreeterFactory.buildGreeter();
-		System.out.println(greeter.greeting());
+		String greeting;
+		if (args.length > 0) {
+			greeting = greeter.greeting(args[0]);
+		} else {
+			greeting = greeter.greeting();
+		}
+		System.out.println(greeting);
 	}
 
 }
